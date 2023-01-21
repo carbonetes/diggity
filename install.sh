61ec1918064e58e376bd2398b3eb9bb4b0b56b6e
Jayson Santos <69297577+jaysonsantos05@users.noreply.github.com>
2023-01-21 15:59:32 +0800

    feat: Add Checksum for sha256 in install.sh

--- install.sh
+++ install.sh
@@ -101,6 +101,28 @@ install_binary() (
   install "${archive_dir}/$3" "$2"
 )
 
+
+verify_hash_sha256() (
+  asset=$1
+  checksums=$2
+  if [ -z "$checksums" ]; then
+    log_err "hash_sha256_verify checksum file not specified in arg2"
+    return 1
+  fi
+  BASENAME=${asset##*/}
+  checksum_hash=$(grep "${BASENAME}" "${checksums}" 2>/dev/null | tr '\t' ' ' | cut -d ' ' -f 1)
+  if [ -z "$checksum_hash" ]; then
+    echo "unable to find checksum for '${asset}' in '${checksums}'"
+    return 1
+  fi
+  asset_hash=$(shasum -a 256 "$asset" 2>/dev/null) 
+  asset_hash=$(echo "$asset_hash" | cut -d ' ' -f 1)
+  if [ "$checksum_hash" != "$asset_hash" ]; then
+    echo "checksum for '${asset##*/}' did not verify ${checksum_hash} vs $asset_hash"
+    return 1
+  fi
+)
+
 extract() (
   archive=$1
   case "$1" in
@@ -147,20 +169,33 @@ install_diggity() {
 
     # init filename for binary
     file_name="${repo}_${version#v}_${os}_${arch}.${format}"
-    downloaded_file="${downloadFolder}/${file_name}"
+    downloaded_asset_file="${downloadFolder}/${file_name}"
     asset_uri="${githubUrl}/${owner}/${repo}/releases/download/${version}/${file_name}"
 
-    echo "[1/3] Download ${asset_uri} to tmp folder"
-    rm -f ${downloaded_file}
-    curl --fail --location --output "${downloaded_file}" "${asset_uri}"
+    echo "[1/4] Download ${asset_uri} to tmp folder"
+    rm -f ${downloaded_asset_file}
+    curl --fail --location --output "${downloaded_asset_file}" "${asset_uri}"
 
-    echo "[2/3] Install ${repo} to the ${executable_folder}"
 
-    install_binary "${downloaded_file}" "${executable_folder}" "${final_binary}"
+    # init filename for checksum
+    file_name="${repo}_${version#v}_checksums.txt"
+    downloaded_checksum_file="${downloadFolder}/${file_name}"
+    checksum_uri="${githubUrl}/${owner}/${repo}/releases/download/${version}/${file_name}"
+
+    echo "[2/4] Download ${checksum_uri} for checksum"
+
+    rm -f ${checksums_filepath}
+    curl --fail --location --output "${downloaded_checksum_file}" "${checksum_uri}"
+    verify_hash_sha256 "${downloaded_asset_file}" "${downloaded_checksum_file}"
+
+
+    echo "[3/4] Install ${repo} to the ${executable_folder}"
+
+    install_binary "${downloaded_asset_file}" "${executable_folder}" "${final_binary}"
     exe=${executable_folder}/${repo}
     chmod +x ${exe}
 
-    echo "[3/3] Set environment variables"
+    echo "[4/4] Set environment variables"
     echo "${repo} was installed successfully to ${exe}"
     if command -v $repo --version >/dev/null; then
         echo "Run '$repo --help' to get started"
