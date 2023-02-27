257593cab4cfae91dfb44f6dd0ff2bd0693377f7
DAGS-PROPERTY\Dags <renan.dagang@carbonetes.com>
2023-02-27 13:49:15 +0800

    feat: added hex parser to sbom

--- internal/sbom.go
+++ internal/sbom.go
@@ -48,6 +48,7 @@ var (
 		parser.FindHackagePackagesFromContent,
 		parser.FindCargoPackagesFromContent,
 		parser.FindConanPackagesFromContent,
+		parser.FindHexPackagesFromContent,
 		parser.ParseDistro,
 		parser.ParseDockerProperties,
 		secret.Search,
