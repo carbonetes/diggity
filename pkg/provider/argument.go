949be15d74a4574f5a527653fee4060eff97e616
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-04-20 18:31:27 +0800

    refactor: update registry auth provider

--- pkg/provider/argument.go
+++ pkg/provider/argument.go
@@ -2,7 +2,6 @@ package provider
 
 import (
 	"github.com/carbonetes/diggity/pkg/model"
-	"github.com/docker/docker/api/types"
 )
 
 // NewArguments returns a new model.Arguments struct with all fields initialized to their zero values.
@@ -27,22 +26,4 @@ func NewArguments() *model.Arguments {
 		RegistryToken:       new(string),
 		Provenance:          new(string),
 	}
-}
-
-// NewRegistryAuth returns a new types.AuthConfig struct with the values set from the given model.Arguments struct.
-func NewRegistryAuth(arguments *model.Arguments) *types.AuthConfig {
-	return &types.AuthConfig{
-		Username:      *setValue(arguments.RegistryUsername),
-		Password:      *setValue(arguments.RegistryPassword),
-		RegistryToken: *setValue(arguments.RegistryToken),
-		ServerAddress: *setValue(arguments.RegistryURI),
-	}
-}
-
-// setValue returns the value of the given string pointer if it is not nil, otherwise it returns nil.
-func setValue(value *string) *string {
-	if value != nil {
-		return value
-	}
-	return nil
 }
---
+++ pkg/provider/registry_auth.go
@@ -0,0 +1,24 @@
+package provider
+
+import (
+	"github.com/carbonetes/diggity/pkg/model"
+	"github.com/docker/docker/api/types"
+)
+
+// NewRegistryAuth returns a new types.AuthConfig struct with the values set from the given model.Arguments struct.
+func NewRegistryAuth(arguments *model.Arguments) *types.AuthConfig {
+	return &types.AuthConfig{
+		Username:      *setValue(arguments.RegistryUsername),
+		Password:      *setValue(arguments.RegistryPassword),
+		RegistryToken: *setValue(arguments.RegistryToken),
+		ServerAddress: *setValue(arguments.RegistryURI),
+	}
+}
+
+// setValue returns the value of the given string pointer if it is not nil, otherwise it returns nil.
+func setValue(value *string) *string {
+	if value != nil {
+		return value
+	}
+	return nil
+}
