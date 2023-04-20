cbd33fa76522093d78fd9ed5b0b70e71b144d0de
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-04-20 19:59:22 +0800

    refactor: update unit test for init parser

--- pkg/parser/bom/init_parser_test.go
+++ pkg/parser/bom/init_parser_test.go
@@ -1,63 +1,45 @@
 package bom
 
 import (
-	"reflect"
 	"testing"
 
-	"github.com/carbonetes/diggity/pkg/model"
-)
-
-type (
-	InitParsersResult struct {
-		_argument *model.Arguments
-		expected  *model.Arguments
-	}
+	"github.com/carbonetes/diggity/pkg/docker"
+	"github.com/carbonetes/diggity/pkg/provider"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestInitParsers(t *testing.T) {
-	argImage1 := "test-image"
-	argImage2 := "test-image/test"
-	argImage3 := "test-image/test:latest"
-	argImage4 := "test-image/testt:xyz"
-	argImage5 := "alpine"
 
-	boolTrue := true
-	boolFalse := false
+	// Test case 1: Image argument provided
+	arg1 := provider.NewArguments()
+	arg1.Image = stringPtr("alpine")
 
-	arguments1 := model.Arguments{
-		Image:              &argImage1,
-		DisableFileListing: &boolTrue,
-	}
-	arguments2 := model.Arguments{
-		Image:              &argImage2,
-		DisableFileListing: &boolTrue,
-	}
-	arguments3 := model.Arguments{
-		Image:              &argImage3,
-		DisableFileListing: &boolFalse,
-	}
-	arguments4 := model.Arguments{
-		Image:              &argImage4,
-		DisableFileListing: &boolTrue,
-	}
-	arguments5 := model.Arguments{
-		Image:              &argImage5,
-		DisableFileListing: &boolFalse,
+	InitParsers(*arg1)
+	if !assert.DirExists(t, *Target) {
+		t.Errorf("Target was not set correctly '%s'", *Target)
 	}
 
-	tests := []InitParsersResult{
-		{&arguments1, &arguments1},
-		{&arguments2, &arguments2},
-		{&arguments3, &arguments3},
-		{&arguments4, &arguments4},
-		{&arguments5, &arguments5},
+	// Test case 2: Dir argument provided
+	arg2 := provider.NewArguments()
+	arg2.Dir = stringPtr(".")
+
+	InitParsers(*arg2)
+	if !assert.DirExists(t, *Target) {
+		t.Errorf("Target was not set correctly '%s'", *Target)
 	}
 
-	for _, test := range tests {
-		InitParsers(*test._argument)
+	// Test case 3: Tar argument provided
+	tarFile := docker.SaveImageToTar(stringPtr("alpine"))
+	arg3 := provider.NewArguments()
+	arg3.Tar = stringPtr(tarFile.Name())
 
-		if !reflect.DeepEqual(Arguments, test.expected) {
-			t.Errorf("Test Failed: Arguments must be instantiated from &arguments.")
-		}
+	InitParsers(*arg3)
+	if !assert.DirExists(t, *Target) {
+		t.Errorf("Target was not set correctly '%s'", *Target)
 	}
+}
+
+// Helper function to create a string pointer
+func stringPtr(s string) *string {
+	return &s
 }
