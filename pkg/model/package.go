0f37c168d363c6c3e4612e93e42316cca5c27282
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-04-28 15:36:47 +0800

    refactor: include vulnerabilities field in package model

--- pkg/model/package.go
+++ pkg/model/package.go
@@ -1,18 +1,21 @@
 package model
 
+import "github.com/carbonetes/jacked/pkg/core/model"
+
 // Package actual package found
 type Package struct {
-	ID          string      `json:"id"`
-	Name        string      `json:"name"`
-	Type        string      `json:"type"`
-	Version     string      `json:"version"`
-	Path        string      `json:"path"`
-	Locations   []Location  `json:"locations"`
-	Description string      `json:"description,omitempty"`
-	Licenses    []string    `json:"licenses,omitempty"`
-	CPEs        []string    `json:"cpes"`
-	PURL        PURL        `json:"purl"`
-	Metadata    interface{} `json:"metadata"`
+	ID              string                 `json:"id"`
+	Name            string                 `json:"name"`
+	Type            string                 `json:"type"`
+	Version         string                 `json:"version"`
+	Path            string                 `json:"path"`
+	Locations       []Location             `json:"locations"`
+	Description     string                 `json:"description,omitempty"`
+	Licenses        []string               `json:"licenses,omitempty"`
+	CPEs            []string               `json:"cpes"`
+	PURL            PURL                   `json:"purl"`
+	Metadata        interface{}            `json:"metadata"`
+	Vulnerabilities *[]model.Vulnerability `json:"vulnerabilities,omitempty"`
 }
 
 // PURL - Package URL
