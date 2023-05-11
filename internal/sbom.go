83ee683d7c5659380ce1f38ae474dcf683451498
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-05-11 11:57:26 +0800

    refactor: create common parser functions initiation

--- internal/sbom.go
+++ internal/sbom.go
@@ -3,69 +3,15 @@ package sbom
 import (
 	"github.com/carbonetes/diggity/internal/logger"
 	"github.com/carbonetes/diggity/internal/output"
-	"github.com/carbonetes/diggity/internal/secret"
 	"github.com/carbonetes/diggity/internal/slsa"
 	"github.com/carbonetes/diggity/internal/ui"
 	"github.com/carbonetes/diggity/pkg/model"
-	"github.com/carbonetes/diggity/pkg/parser/alpine"
-	"github.com/carbonetes/diggity/pkg/parser/alpmdb"
+	"github.com/carbonetes/diggity/pkg/parser"
 	"github.com/carbonetes/diggity/pkg/parser/bom"
-	"github.com/carbonetes/diggity/pkg/parser/cargo"
-	"github.com/carbonetes/diggity/pkg/parser/composer"
-	"github.com/carbonetes/diggity/pkg/parser/conan"
-	"github.com/carbonetes/diggity/pkg/parser/dart"
-	"github.com/carbonetes/diggity/pkg/parser/debian"
-	"github.com/carbonetes/diggity/pkg/parser/distro"
-	"github.com/carbonetes/diggity/pkg/parser/docker"
-	"github.com/carbonetes/diggity/pkg/parser/gem"
-	"github.com/carbonetes/diggity/pkg/parser/golang"
-	"github.com/carbonetes/diggity/pkg/parser/hackage"
-	"github.com/carbonetes/diggity/pkg/parser/hex"
-	"github.com/carbonetes/diggity/pkg/parser/java"
-	"github.com/carbonetes/diggity/pkg/parser/npm"
-	"github.com/carbonetes/diggity/pkg/parser/nuget"
-	"github.com/carbonetes/diggity/pkg/parser/portage"
-	"github.com/carbonetes/diggity/pkg/parser/python"
-	"github.com/carbonetes/diggity/pkg/parser/rpm"
-	"github.com/carbonetes/diggity/pkg/parser/swift"
 	"github.com/carbonetes/diggity/pkg/parser/util"
 )
 
-type (
-	parsers []func(*bom.ParserRequirements)
-)
-
-var (
-	// FindFunctions is a collection of the find content functions of all parsers.
-	FindFunctions = parsers{
-		alpine.FindAlpinePackagesFromContent,
-		debian.FindDebianPackagesFromContent,
-		java.FindJavaPackagesFromContent,
-		npm.FindNpmPackagesFromContent,
-		composer.FindComposerPackagesFromContent,
-		python.FindPythonPackagesFromContent,
-		gem.FindGemPackagesFromContent,
-		rpm.FindRpmPackagesFromContent,
-		dart.FindDartPackagesFromContent,
-		nuget.FindNugetPackagesFromContent,
-		golang.FindGoModPackagesFromContent,
-		golang.FindGoBinPackagesFromContent,
-		hackage.FindHackagePackagesFromContent,
-		cargo.FindCargoPackagesFromContent,
-		conan.FindConanPackagesFromContent,
-		portage.FindPortagePackagesFromContent,
-		hex.FindHexPackagesFromContent,
-		swift.FindSwiftPackagesFromContent,
-		distro.ParseDistro,
-		docker.ParseDockerProperties,
-		secret.Search,
-		alpmdb.FindAlpmdbPackagesFromContent,
-	}
-)
-
-var (
-	log = logger.GetLogger()
-)
+var log = logger.GetLogger()
 
 // Start SBOM extraction
 func Start(arguments *model.Arguments) {
@@ -79,8 +25,8 @@ func Start(arguments *model.Arguments) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	requirements.WG.Add(len(FindFunctions))
-	for _, parser := range FindFunctions {
+	requirements.WG.Add(len(parser.FindFunctions))
+	for _, parser := range parser.FindFunctions {
 		go parser(requirements)
 	}
 	requirements.WG.Wait()
