8ccfeecd3cc920332cd3e58ab0a83638a74d38e2
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-06-01 12:08:11 +0800

    refactor: rework debian parser unit test

---
+++ pkg/parser/debian/debian_test.go
@@ -0,0 +1,58 @@
+package debian_test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/carbonetes/diggity/pkg/model"
+	"github.com/carbonetes/diggity/pkg/parser/bom"
+	"github.com/carbonetes/diggity/pkg/parser/debian"
+)
+
+var (
+	args   = model.NewArguments()
+	target = filepath.Join("..", "..", "..", "docs", "references", "debian")
+)
+
+func TestDart(t *testing.T) {
+	if _, err := os.Stat(target); os.IsNotExist(err) {
+		t.Error(errors.New("Debian reference not found"))
+		t.FailNow()
+	}
+	args.Dir = &target
+
+	req, err := bom.InitParsers(args)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.WG.Add(1)
+	debian.FindDebianPackagesFromContent(req)
+	req.WG.Wait()
+	if len(*req.Errors) > 0 {
+		for _, err := range *req.Errors {
+			t.Error(err)
+		}
+	}
+
+	if len(*req.SBOM.Packages) == 0 {
+		t.Error(errors.New("No package has been found!"))
+	}
+
+	for index, p := range *req.SBOM.Packages {
+		if len(p.ID) == 0 {
+			t.Error(errors.New("Empty package id has been detected at index " + fmt.Sprint(index)))
+		}
+		if len(p.Name) == 0 {
+			t.Error(errors.New("Empty package name has been detected at index " + fmt.Sprint(index)))
+		}
+		if len(p.CPEs) == 0 {
+			t.Error(errors.New("Empty package cpe has been detected at index " + fmt.Sprint(index)))
+		}
+		if p.Metadata == nil {
+			t.Error(errors.New("Nil package metadata has been detected at index " + fmt.Sprint(index)))
+		}
+	}
+}
