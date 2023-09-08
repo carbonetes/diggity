c3009d35ff180d06efb0519d332098c463f9a408
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-09-08 12:33:16 +0800

    feat: add sbt version value checking

--- pkg/parser/java/sbt/parser.go
+++ pkg/parser/java/sbt/parser.go
@@ -24,15 +24,17 @@ func parserSbtPackages(location *model.Location, req *bom.ParserRequirements) {
 	defer file.Close()
 	
 	scanner := bufio.NewScanner(file)
+	var lines []string
 	for scanner.Scan() {
 		properties := scanner.Text()
-
+		lines = append(lines, properties)
 		if !strings.Contains(properties, "%") {
 			continue
 		}
 
 		var sbtMetadata metadata.SbtMetadata
 		values := strings.SplitN(properties, "%", 3)
+
 		
 		if len(values) != 3 && len(values) <= 4 {
 			continue
@@ -46,14 +48,16 @@ func parserSbtPackages(location *model.Location, req *bom.ParserRequirements) {
 			}
 		}
 
+		if !strings.Contains(values[2], "\"") {
+			values[2] = variableAsVersion(lines, values[2])
+		}
+
 		sbtMetadata.Vendor = removeDoubleQoute(values[0])
 		sbtMetadata.Name = removeDoubleQoute(values[1])
 		sbtMetadata.Version = removeDoubleQoute(values[2])
-		if strings.Contains(sbtMetadata.Version, "%") {
-			splitVersionConfig := strings.SplitN(sbtMetadata.Version, "%", 2)
-			sbtMetadata.Version = splitVersionConfig[0]
-			sbtMetadata.Config = splitVersionConfig[1]
-		}
+
+		// Check if version has config value
+		splitConfigValue(sbtMetadata)
 
 		sbtMetadata.Version = sanitizeVersion(sbtMetadata.Version)
 
@@ -68,13 +72,35 @@ func parserSbtPackages(location *model.Location, req *bom.ParserRequirements) {
 	}
 }
 
-func removeDoubleQoute(word string) string{
+func removeDoubleQoute(word string) string {
 	word = strings.ReplaceAll(word, "\"", "")
-	return strings.ReplaceAll(word, ",", "")
+	wordWithoutSpaces := strings.ReplaceAll(word, " ", "")
+	return strings.ReplaceAll(wordWithoutSpaces, ",", "")
 }
 
 func sanitizeVersion(version string) string {
 	// Define a regular expression to match allowed characters for special cases sbt dependencies
 	allowedPattern := regexp.MustCompile(`[^a-zA-Z0-9._-]+`) // allowed ^._-, all numbers and letters
 	return allowedPattern.ReplaceAllString(version, "")
+}
+
+func splitConfigValue(sbtMetadata metadata.SbtMetadata) {
+	
+	if strings.Contains(sbtMetadata.Version, "%") {
+		splitVersionConfig := strings.SplitN(sbtMetadata.Version, "%", 2)
+		sbtMetadata.Version = splitVersionConfig[0]
+		sbtMetadata.Config = splitVersionConfig[1]
+	}
+}
+
+func variableAsVersion(lines []string, variableAsVersion string) string {
+	
+	for _, line := range lines {
+		if strings.Contains(line, variableAsVersion) && strings.Contains(line, "=") {
+			
+			splitVersionValue := strings.SplitN(line, "=", 2)
+			return sanitizeVersion(splitVersionValue[1])
+		}
+	}
+	return ""
 }
