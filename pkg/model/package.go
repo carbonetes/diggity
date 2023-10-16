6a2cab3aec236291ef8af6874022b284567987a1
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-10-16 14:54:24 +0800

    feature: add  fields for package origin, parser, distro, and language

--- pkg/model/package.go
+++ pkg/model/package.go
@@ -2,12 +2,24 @@ package model
 
 import "github.com/carbonetes/jacked/pkg/core/model"
 
+// PackageType represents the source/type of the package.
+type PackageType string
+
+const (
+	OSPackage    PackageType = "os"
+	ApplicationPackage PackageType = "application"
+)
+
 // Package actual package found
 type Package struct {
 	ID              string                 `json:"id"`
 	Name            string                 `json:"name"`
 	Type            string                 `json:"type"`
 	Version         string                 `json:"version"`
+	PackageOrigin   PackageType            `json:"origin"`
+	Parser          string                 `json:"parser"`
+	Distro          string                 `json:"distro"`
+	Language        string                 `json:"language"`
 	Path            string                 `json:"path"`
 	Locations       []Location             `json:"locations"`
 	Description     string                 `json:"description,omitempty"`
