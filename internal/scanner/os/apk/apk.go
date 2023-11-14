934ef2475c2b93ed2a8ae4b38da110a4394004e3
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-11-14 08:43:28 +0800

    feat: add os apk scanner sub-module

---
+++ internal/scanner/os/apk/apk.go
@@ -0,0 +1,37 @@
+package apk
+
+import (
+	"log"
+
+	"github.com/carbonetes/diggity/pkg/stream"
+	"github.com/carbonetes/diggity/pkg/types"
+)
+
+const Distro = "alpine"
+
+var (
+	Type    = "apk"
+)
+
+func Scan(data interface{}) interface{} {
+	manifest, ok := data.(types.ManifestFile)
+
+	if !ok {
+		log.Fatal("Apk Handler received unknown type")
+		return nil
+	}
+
+	attributes, err := readManifest(manifest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, attribute := range attributes {
+		component := newComponent(attribute)
+		if len(component.ID) == 0 {
+			continue
+		}
+		stream.AddComponent(component)
+	}
+	return data
+}
