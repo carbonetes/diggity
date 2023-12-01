3edbf7434fc096d55688561c5061c698f58541c7
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-12-01 17:13:49 +0800

    feat: add language/gradle scanner

---
+++ internal/scanner/language/gradle/gradle.go
@@ -0,0 +1,60 @@
+package gradle
+
+import (
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/carbonetes/diggity/internal/logger"
+	"github.com/carbonetes/diggity/pkg/stream"
+	"github.com/carbonetes/diggity/pkg/types"
+)
+
+const Type string = "gradle"
+
+var (
+	Manifests = []string{"buildscript-gradle.lockfile", ".build.gradle"}
+	log       = logger.GetLogger()
+)
+
+func CheckRelatedFile(file string) (string, bool) {
+	if slices.Contains(Manifests, filepath.Base(file)) {
+		return Type, true
+	}
+	return "", false
+}
+
+func Scan(data interface{}) interface{} {
+	manifest, ok := data.(types.ManifestFile)
+	if !ok {
+		log.Fatal("Gradle Handler received unknown type")
+	}
+
+	lines := strings.Split(string(manifest.Content), "\n")
+	for _, line := range lines {
+		if !strings.Contains(line, ":") {
+			continue
+		}
+
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		attributes := strings.SplitN(line, ":", 3)
+		if len(attributes) < 3 {
+			continue
+		}
+
+		metadata := Metadata{
+			Vendor:  attributes[0],
+			Name:    attributes[1],
+			Version: strings.ReplaceAll(attributes[2], "=classpath", ""),
+		}
+
+		component := types.NewComponent(metadata.Name, metadata.Version, Type, manifest.Path, "", metadata)
+
+		stream.AddComponent(component)
+	}
+	return data
+}
---
+++ internal/scanner/language/gradle/metadata.go
@@ -0,0 +1,7 @@
+package gradle
+
+type Metadata struct {
+	Vendor  string `json:"vendor"`
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
