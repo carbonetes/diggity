31705a49e0c81fdf5b60aef658df9a782fae860c
ianbautista0206 <ian.bautista@carbonetes.com>
2024-04-30 11:30:06 +0800

    fix: pom dep not included in bom

--- pkg/scanner/language/maven/maven.go
+++ pkg/scanner/language/maven/maven.go
@@ -42,6 +42,40 @@ func scan(payload types.Payload) {
 		return
 	}
 
+	if len(metadata.Dependencies) > 0 {
+		for _, dependency := range metadata.Dependencies {
+			if dependency.GroupID == "" || dependency.ArtifactID == "" || dependency.Version == "" {
+				continue
+			}
+
+			c := component.New(dependency.ArtifactID, dependency.Version, Type)
+
+			cpes := cpe.NewCPE23(c.Name, c.Name, c.Version, Type)
+			if len(cpes) > 0 {
+				for _, cpe := range cpes {
+					component.AddCPE(c, cpe)
+				}
+			}
+
+			// Correction for PackageURL
+			c.PackageURL = "pkg:maven/" + dependency.GroupID + "/" + dependency.ArtifactID + "@" + dependency.Version
+
+			component.AddOrigin(c, manifest.Path)
+			component.AddType(c, Type)
+
+			rawMetadata, err := helper.ToJSON(dependency)
+			if err != nil {
+				log.Errorf("Failed to convert metadata to JSON: %v", err)
+			}
+
+			if len(rawMetadata) > 0 {
+				component.AddRawMetadata(c, rawMetadata)
+			}
+
+			cdx.AddComponent(c, payload.Address)
+		}
+	}
+
 	if metadata.ArtifactID == "" || metadata.Version == "" {
 		return
 	}
